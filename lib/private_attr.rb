beac9182b13a65bc8a60ddff7d80d330fa22e3d6
Piotr Szotkowski <chastell@chastell.net>
2015-01-30 21:41:19 +0100

    Make PrivateAttr warning-free

--- lib/private_attr.rb
+++ lib/private_attr.rb
@@ -2,32 +2,32 @@ require "private_attr/version"
 
 module PrivateAttr
   def private_attr_accessor *attr
-    private_attr_reader *attr
-    private_attr_writer *attr
+    private_attr_reader(*attr)
+    private_attr_writer(*attr)
   end
 
   def private_attr_reader *attr
-    attr_reader *attr
-    private *attr
+    attr_reader(*attr)
+    private(*attr)
   end
 
   def private_attr_writer *attr
-    attr_writer *attr
-    private *attr.map { |a| "#{a}=" }
+    attr_writer(*attr)
+    private(*attr.map { |a| "#{a}=" })
   end
 
   def protected_attr_accessor *attr
-    protected_attr_reader *attr
-    protected_attr_writer *attr
+    protected_attr_reader(*attr)
+    protected_attr_writer(*attr)
   end
 
   def protected_attr_reader *attr
-    attr_reader *attr
-    protected *attr
+    attr_reader(*attr)
+    protected(*attr)
   end
 
   def protected_attr_writer *attr
-    attr_writer *attr
-    protected *attr.map { |a| "#{a}=" }
+    attr_writer(*attr)
+    protected(*attr.map { |a| "#{a}=" })
   end
 end
