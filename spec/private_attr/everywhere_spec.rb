00067baf89ec348a85ba27c92bb8d816de0fc2a0
Piotr Szotkowski <chastell@chastell.net>
2015-02-09 21:48:07 +0100

    Make private_attr/everywhere tests run in order

--- spec/private_attr/everywhere_spec.rb
+++ spec/private_attr/everywhere_spec.rb
@@ -1,6 +1,11 @@
 require 'minitest/autorun'
 
 describe 'private_attr/everywhere' do
+  # these particular tests must be ran in order
+  def self.test_order
+    :alpha
+  end
+
   it 'requiring only private_attr keeps Classes and Modules intact' do
     require 'private_attr'
     -> { class  Cla; private_attr_reader :priv; end }.must_raise NoMethodError
