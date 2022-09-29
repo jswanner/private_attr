eb2b58489b061d2680ed48e54d1bdee197f95821
Jacob Swanner <jacob@envylabs.com>
2022-09-29 15:08:06 -0700

    Resolve minitest deprecation warnings

--- spec/private_attr_spec.rb
+++ spec/private_attr_spec.rb
@@ -1,20 +1,20 @@
-require 'minitest/autorun'
-require 'private_attr'
+require "minitest/autorun"
+require "private_attr"
 
 class Dummy
   def read_accessor
     accessor
   end
 
-  def read_accessor_other other
+  def read_accessor_other(other)
     other.accessor
   end
 
-  def write_accessor value
+  def write_accessor(value)
     self.accessor = value
   end
 
-  def write_accessor_other other, value
+  def write_accessor_other(other, value)
     other.accessor = value
   end
 
@@ -22,15 +22,15 @@ class Dummy
     reader
   end
 
-  def read_reader_other other
+  def read_reader_other(other)
     other.reader
   end
 
-  def write_writer value
+  def write_writer(value)
     self.writer = value
   end
 
-  def write_writer_other other, value
+  def write_writer_other(other, value)
     other.writer = value
   end
 
@@ -40,7 +40,7 @@ class Dummy
     aliased
   end
 
-  def call_aliased_other other
+  def call_aliased_other(other)
     other.aliased
   end
 end
@@ -64,123 +64,123 @@ end
 describe PrivateAttr do
   let(:dummy) { dummy_class.new }
   let(:other) { dummy_class.new }
-  let(:old_value) { 'old value' }
-  let(:new_value) { 'new value' }
+  let(:old_value) { "old value" }
+  let(:new_value) { "new value" }
 
-  describe 'private_attr_accessor' do
+  describe "private_attr_accessor" do
     let(:dummy_class) { PrivateDummy }
 
-    before { dummy.instance_variable_set '@accessor', old_value }
+    before { dummy.instance_variable_set "@accessor", old_value }
 
-    it 'allows attribute to be read internally' do
-      dummy.read_accessor.must_equal old_value
+    it "allows attribute to be read internally" do
+      _(dummy.read_accessor).must_equal old_value
     end
 
-    it 'allows attribute to be written internally' do
+    it "allows attribute to be written internally" do
       dummy.write_accessor new_value
-      dummy.instance_variable_get('@accessor').must_equal new_value
+      _(dummy.instance_variable_get("@accessor")).must_equal new_value
     end
 
-    it 'raises Error when read externally' do
-      -> { dummy.accessor }.must_raise NoMethodError
+    it "raises Error when read externally" do
+      _{ dummy.accessor }.must_raise NoMethodError
     end
 
-    it 'raises Error when written externally' do
-      -> { dummy.accessor = new_value }.must_raise NoMethodError
+    it "raises Error when written externally" do
+      _{ dummy.accessor = new_value }.must_raise NoMethodError
     end
 
-    it 'raises Error when read by other' do
-      -> { dummy.read_accessor_other other }.must_raise NoMethodError
+    it "raises Error when read by other" do
+      _{ dummy.read_accessor_other other }.must_raise NoMethodError
     end
 
-    it 'raises Error when written by other' do
-      -> { dummy.write_accessor_other other, new_value }.must_raise NoMethodError
-    end
-  end
-
-  describe 'private_attr_reader' do
-    let(:dummy_class) { PrivateDummy }
-
-    before { dummy.instance_variable_set '@reader', old_value }
-
-    it 'allows attribute to be read internally' do
-      dummy.read_reader.must_equal old_value
-    end
-
-    it 'raises Error when written internally' do
-      -> { dummy.write_reader new_value }.must_raise NoMethodError
-    end
-
-    it 'raises Error when read externally' do
-      -> { dummy.reader }.must_raise NoMethodError
-    end
-
-    it 'raises Error when written externally' do
-      -> { dummy.reader = new_value }.must_raise NoMethodError
-    end
-
-    it 'raises Error when read by other' do
-      -> { dummy.read_reader_other other }.must_raise NoMethodError
-    end
-
-    it 'raises Error when written by other' do
-      -> { dummy.write_reader_other other, new_value }.must_raise NoMethodError
+    it "raises Error when written by other" do
+      _{ dummy.write_accessor_other other, new_value }.must_raise NoMethodError
     end
   end
 
-  describe 'private_attr_writer' do
+  describe "private_attr_reader" do
     let(:dummy_class) { PrivateDummy }
 
-    before { dummy.instance_variable_set '@writer', old_value }
+    before { dummy.instance_variable_set "@reader", old_value }
 
-    it 'raises Error when read internally' do
-      -> { dummy.read_writer }.must_raise NoMethodError
+    it "allows attribute to be read internally" do
+      _(dummy.read_reader).must_equal old_value
     end
 
-    it 'allows attribute to be written internally' do
+    it "raises Error when written internally" do
+      _{ dummy.write_reader new_value }.must_raise NoMethodError
+    end
+
+    it "raises Error when read externally" do
+      _{ dummy.reader }.must_raise NoMethodError
+    end
+
+    it "raises Error when written externally" do
+      _{ dummy.reader = new_value }.must_raise NoMethodError
+    end
+
+    it "raises Error when read by other" do
+      _{ dummy.read_reader_other other }.must_raise NoMethodError
+    end
+
+    it "raises Error when written by other" do
+      _{ dummy.write_reader_other other, new_value }.must_raise NoMethodError
+    end
+  end
+
+  describe "private_attr_writer" do
+    let(:dummy_class) { PrivateDummy }
+
+    before { dummy.instance_variable_set "@writer", old_value }
+
+    it "raises Error when read internally" do
+      _{ dummy.read_writer }.must_raise NoMethodError
+    end
+
+    it "allows attribute to be written internally" do
       dummy.write_writer new_value
-      dummy.instance_variable_get('@writer').must_equal new_value
+      _(dummy.instance_variable_get("@writer")).must_equal new_value
     end
 
-    it 'raises Error when read externally' do
-      -> { dummy.writer }.must_raise NoMethodError
+    it "raises Error when read externally" do
+      _{ dummy.writer }.must_raise NoMethodError
     end
 
-    it 'raises Error when written externally' do
-      -> { dummy.writer = new_value }.must_raise NoMethodError
+    it "raises Error when written externally" do
+      _{ dummy.writer = new_value }.must_raise NoMethodError
     end
 
-    it 'raises Error when read by other' do
-      -> { dummy.read_writer_other other }.must_raise NoMethodError
+    it "raises Error when read by other" do
+      _{ dummy.read_writer_other other }.must_raise NoMethodError
     end
 
-    it 'raises Error when written by other' do
-      -> { dummy.write_writer_other other, new_value }.must_raise NoMethodError
+    it "raises Error when written by other" do
+      _{ dummy.write_writer_other other, new_value }.must_raise NoMethodError
     end
   end
 
-  describe 'private_alias_method' do
+  describe "private_alias_method" do
     let(:dummy_class) { PrivateDummy }
 
-    before { dummy.instance_variable_set '@original', old_value }
+    before { dummy.instance_variable_set "@original", old_value }
 
-    it 'allows alias to be called internally' do
-      dummy.call_aliased.must_equal old_value
+    it "allows alias to be called internally" do
+      _(dummy.call_aliased).must_equal old_value
     end
 
-    it 'raises Error when alias is called externally' do
-      -> { dummy.aliased other }.must_raise NoMethodError
+    it "raises Error when alias is called externally" do
+      _{ dummy.aliased other }.must_raise NoMethodError
     end
 
-    it 'raises Error when alias is called by other' do
-      -> { dummy.call_aliased_other other }.must_raise NoMethodError
+    it "raises Error when alias is called by other" do
+      _{ dummy.call_aliased_other other }.must_raise NoMethodError
     end
 
-    it 'allows the original method to be called externally' do
-      dummy.original.must_equal old_value
+    it "allows the original method to be called externally" do
+      _(dummy.original).must_equal old_value
     end
 
-    describe 'when the original method is overridden' do
+    describe "when the original method is overridden" do
       let(:dummy_class) do
         mod = Module.new do
           def original
@@ -190,146 +190,146 @@ describe PrivateAttr do
         Class.new(PrivateDummy).send(:include, mod)
       end
 
-      it 'allows alias to be called internally' do
-        dummy.call_aliased.must_equal old_value
+      it "allows alias to be called internally" do
+        _(dummy.call_aliased).must_equal old_value
       end
 
-      it 'raises Error when alias is called externally' do
-        -> { dummy.aliased }.must_raise NoMethodError
+      it "raises Error when alias is called externally" do
+        _{ dummy.aliased }.must_raise NoMethodError
       end
 
-      it 'raises Error when alias is called by other' do
-        -> { dummy.call_aliased_other other }.must_raise NoMethodError
+      it "raises Error when alias is called by other" do
+        _{ dummy.call_aliased_other other }.must_raise NoMethodError
       end
 
-      it 'allows the overridden method to be called externally' do
-        dummy.original.must_equal old_value.upcase
+      it "allows the overridden method to be called externally" do
+        _(dummy.original).must_equal old_value.upcase
       end
     end
   end
 
-  describe 'protected_attr_accessor' do
+  describe "protected_attr_accessor" do
     let(:dummy_class) { ProtectedDummy }
 
-    before { dummy.instance_variable_set '@accessor', old_value }
+    before { dummy.instance_variable_set "@accessor", old_value }
 
-    it 'allows attribute to be read internally' do
-      dummy.read_accessor.must_equal old_value
+    it "allows attribute to be read internally" do
+      _(dummy.read_accessor).must_equal old_value
     end
 
-    it 'allows attribute to be written internally' do
+    it "allows attribute to be written internally" do
       dummy.write_accessor new_value
-      dummy.instance_variable_get('@accessor').must_equal new_value
+      _(dummy.instance_variable_get("@accessor")).must_equal new_value
     end
 
-    it 'raises Error when read externally' do
-      -> { dummy.accessor }.must_raise NoMethodError
+    it "raises Error when read externally" do
+      _{ dummy.accessor }.must_raise NoMethodError
     end
 
-    it 'raises Error when written externally' do
-      -> { dummy.accessor = new_value }.must_raise NoMethodError
+    it "raises Error when written externally" do
+      _{ dummy.accessor = new_value }.must_raise NoMethodError
     end
 
-    it 'allows attribute to be read by other' do
-      other.instance_variable_set '@accessor', new_value
-      dummy.read_accessor_other(other).must_equal new_value
+    it "allows attribute to be read by other" do
+      other.instance_variable_set "@accessor", new_value
+      _(dummy.read_accessor_other(other)).must_equal new_value
     end
 
-    it 'allows attribute to be written by other' do
+    it "allows attribute to be written by other" do
       dummy.write_accessor_other other, new_value
-      other.instance_variable_get('@accessor').must_equal new_value
+      _(other.instance_variable_get("@accessor")).must_equal new_value
     end
   end
 
-  describe 'protected_attr_reader' do
+  describe "protected_attr_reader" do
     let(:dummy_class) { ProtectedDummy }
 
-    before { dummy.instance_variable_set '@reader', old_value }
+    before { dummy.instance_variable_set "@reader", old_value }
 
-    it 'allows attribute to be read internally' do
-      dummy.read_reader.must_equal old_value
+    it "allows attribute to be read internally" do
+      _(dummy.read_reader).must_equal old_value
     end
 
-    it 'raises Error when written internally' do
-      -> { dummy.write_reader new_value }.must_raise NoMethodError
+    it "raises Error when written internally" do
+      _{ dummy.write_reader new_value }.must_raise NoMethodError
     end
 
-    it 'raises Error when read externally' do
-      -> { dummy.reader }.must_raise NoMethodError
+    it "raises Error when read externally" do
+      _{ dummy.reader }.must_raise NoMethodError
     end
 
-    it 'raises Error when written externally' do
-      -> { dummy.reader = new_value }.must_raise NoMethodError
+    it "raises Error when written externally" do
+      _{ dummy.reader = new_value }.must_raise NoMethodError
     end
 
-    it 'allows attribute to be read by other' do
-      other.instance_variable_set '@reader', new_value
-      dummy.read_reader_other(other).must_equal new_value
+    it "allows attribute to be read by other" do
+      other.instance_variable_set "@reader", new_value
+      _(dummy.read_reader_other(other)).must_equal new_value
     end
 
-    it 'raises Error when written by other' do
-      -> { dummy.write_reader_other other, new_value }.must_raise NoMethodError
+    it "raises Error when written by other" do
+      _{ dummy.write_reader_other other, new_value }.must_raise NoMethodError
     end
   end
 
-  describe 'protected_attr_writer' do
+  describe "protected_attr_writer" do
     let(:dummy_class) { ProtectedDummy }
 
-    before { dummy.instance_variable_set '@writer', old_value }
+    before { dummy.instance_variable_set "@writer", old_value }
 
-    it 'raises Error when read internally' do
-      -> { dummy.read_writer }.must_raise NoMethodError
+    it "raises Error when read internally" do
+      _{ dummy.read_writer }.must_raise NoMethodError
     end
 
-    it 'allows attribute to be written internally' do
+    it "allows attribute to be written internally" do
       dummy.write_writer new_value
-      dummy.instance_variable_get('@writer').must_equal new_value
+      _(dummy.instance_variable_get("@writer")).must_equal new_value
     end
 
-    it 'raises Error when read externally' do
-      -> { dummy.writer }.must_raise NoMethodError
+    it "raises Error when read externally" do
+      _{ dummy.writer }.must_raise NoMethodError
     end
 
-    it 'raises Error when written externally' do
-      -> { dummy.writer = new_value }.must_raise NoMethodError
+    it "raises Error when written externally" do
+      _{ dummy.writer = new_value }.must_raise NoMethodError
     end
 
-    it 'raises Error when read by other' do
-      -> { dummy.read_writer_other other }.must_raise NoMethodError
+    it "raises Error when read by other" do
+      _{ dummy.read_writer_other other }.must_raise NoMethodError
     end
 
-    it 'allows attribute to be written by other' do
+    it "allows attribute to be written by other" do
       dummy.write_writer_other other, new_value
-      other.instance_variable_get('@writer').must_equal new_value
+      _(other.instance_variable_get("@writer")).must_equal new_value
     end
   end
 
-  describe 'protected_alias_method' do
+  describe "protected_alias_method" do
     let(:dummy_class) { ProtectedDummy }
-    let(:other_value) { 'other value' }
+    let(:other_value) { "other value" }
 
     before do
-      dummy.instance_variable_set '@original', old_value
-      other.instance_variable_set '@original', other_value
+      dummy.instance_variable_set "@original", old_value
+      other.instance_variable_set "@original", other_value
     end
 
-    it 'allows alias to be called internally' do
-      dummy.call_aliased.must_equal old_value
+    it "allows alias to be called internally" do
+      _(dummy.call_aliased).must_equal old_value
     end
 
-    it 'raises Error when alias is called externally' do
-      -> { dummy.aliased }.must_raise NoMethodError
+    it "raises Error when alias is called externally" do
+      _{ dummy.aliased }.must_raise NoMethodError
     end
 
-    it 'allows alias to be called by other' do
-      dummy.call_aliased_other(other).must_equal other_value
+    it "allows alias to be called by other" do
+      _(dummy.call_aliased_other(other)).must_equal other_value
     end
 
-    it 'allows the original method to be called externally' do
-      dummy.original.must_equal old_value
+    it "allows the original method to be called externally" do
+      _(dummy.original).must_equal old_value
     end
 
-    describe 'when the original method is overridden' do
+    describe "when the original method is overridden" do
       let(:dummy_class) do
         mod = Module.new do
           def original
@@ -339,28 +339,27 @@ describe PrivateAttr do
         Class.new(ProtectedDummy).send(:include, mod)
       end
 
-      it 'allows alias to be called internally' do
-        dummy.call_aliased.must_equal old_value
+      it "allows alias to be called internally" do
+        _(dummy.call_aliased).must_equal old_value
       end
 
-      it 'raises Error when alias is called externally' do
-        -> { dummy.aliased }.must_raise NoMethodError
+      it "raises Error when alias is called externally" do
+        _{ dummy.aliased }.must_raise NoMethodError
       end
 
-      it 'allows alias to be called by other' do
-        dummy.call_aliased_other(other).must_equal other_value
+      it "allows alias to be called by other" do
+        _(dummy.call_aliased_other(other)).must_equal other_value
       end
 
-      it 'allows the overridden method to be called externally' do
-        dummy.original.must_equal old_value.upcase
+      it "allows the overridden method to be called externally" do
+        _(dummy.original).must_equal old_value.upcase
       end
     end
   end
 
-
-  describe 'method visibility' do
-    it 'extending classes does not increase their public APIs' do
-      PrivateDummy.public_methods.wont_include :private_attr_accessor
+  describe "method visibility" do
+    it "extending classes does not increase their public APIs" do
+      _(PrivateDummy.public_methods).wont_include :private_attr_accessor
     end
   end
 end
